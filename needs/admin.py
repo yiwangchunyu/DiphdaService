9f21491e94616adcdd2cf282f420ca45ba9fc0be
yiwangchunyu <yiwangchunyu@163.com>
2019-05-10 14:03:08 +0800

    增加抢单功能

--- needs/admin.py
+++ needs/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from needs.models import Need, Tag, Category
+from needs.models import Need, Tag, Category, Order
 
 
 @admin.register(Need)
@@ -61,6 +61,26 @@ class CategoryAdmin(admin.ModelAdmin):
 
     # list_editable 设置默认可编辑字段
     list_editable = ['name']
+
+    # 设置哪些字段可以点击进入编辑界面
+    # list_display_links = ('id', 'caption')
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    # listdisplay设置要显示在列表中的字段（id字段是Django模型的默认主键）
+    list_display = ('id', 'user_id','need_id','order_status','ctime','mtime')
+
+    #搜索字段
+    search_fields = ('id', 'user_id','need_id')
+
+    # list_per_page设置每页显示多少条记录，默认是100条
+    list_per_page = 50
+
+    # ordering设置默认排序字段，负号表示降序排序
+    # ordering = ('id',)
+
+    # list_editable 设置默认可编辑字段
+    # list_editable = ['name']
 
     # 设置哪些字段可以点击进入编辑界面
     # list_display_links = ('id', 'caption')
